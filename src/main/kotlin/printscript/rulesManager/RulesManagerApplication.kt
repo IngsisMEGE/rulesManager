65ac590cb352df5124f4e732858de8a0ec229ec3
juancruzestevez02 <juan.estevez@ing.austral.edu.ar>
2024-05-02 15:03:49 -0300

    Initial commit

---
+++ src/main/kotlin/printscript/rulesManager/RulesManagerApplication.kt
@@ -0,0 +1,11 @@
+package printscript.rulesManager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RulesManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<RulesManagerApplication>(*args)
+}
---
+++ src/test/kotlin/printscript/rulesManager/RulesManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package printscript.rulesManager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RulesManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
